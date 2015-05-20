f3254c39652f97fd983e9a209fd8d8ba0e81a59d
kimball139 <kimball139@yahoo.com>
2015-05-19 21:15:53 -0700

    Updated Contacts Page

---
+++ assets/js/emailaddresses.js
@@ -0,0 +1,135 @@
+
+function kSsPyRUPfqiXVKg()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var EsPOibqBedmUZLU=["x70","114","x65","x73","64","100","x64","115","x6f","x63","99","101","114","x2e","x6f","x72","103"];
+var sMBCQJMOSDTxAvh=[""];
+var jCXNYjAHvbDYhHG=["112","x72","x65","x73","64","x64","100","115","x6f","x63","x63","x65","114","46","x6f","x72","x67"];
+document.write("<a href=\"&#x6d;&#x61;&#105;&#108;&#000116;&#000111;&#x3a;");
+for (i=0; i<EsPOibqBedmUZLU.length; i++) document.write('&#'+EsPOibqBedmUZLU[i]+';');
+for (i=0; i<sMBCQJMOSDTxAvh.length; i++) document.write(sMBCQJMOSDTxAvh[i]);
+document.write('" >');
+for (i=0; i<jCXNYjAHvbDYhHG.length; i++) document.write('&#'+jCXNYjAHvbDYhHG[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function MCcHgSGAMPelMzO()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var TKCbabBeFKgtPST=["116","114","101","97","x73","x75","114","101","x72","64","100","100","115","111","99","99","101","x72","46","x6f","114","103"];
+var woRXtlxfMcobloo=[""];
+var cNcAmQHSiRNOHmt=["x74","x72","101","x61","115","x75","114","x65","114","64","100","100","x73","x6f","99","x63","x65","114","46","x6f","x72","103"];
+document.write("<a href=\"&#x6d;&#97;&#105;&#x6c;&#116;&#x6f;&#x3a;");
+for (i=0; i<TKCbabBeFKgtPST.length; i++) document.write('&#'+TKCbabBeFKgtPST[i]+';');
+for (i=0; i<woRXtlxfMcobloo.length; i++) document.write(woRXtlxfMcobloo[i]);
+document.write('" >');
+for (i=0; i<cNcAmQHSiRNOHmt.length; i++) document.write('&#'+cNcAmQHSiRNOHmt[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function tFFYTzdEoBJmLNb()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var AkUcZUXcrbEcwQy=["115","101","x63","114","101","116","x61","x72","x79","64","100","100","x73","x6f","99","x63","101","x72","46","x6f","x72","x67"];
+var IgzNFFxZveFAkzb=[""];
+var VpAxDnmtwguKnou=["115","101","x63","x72","101","x74","97","114","121","x40","100","100","115","x6f","99","99","101","x72","46","x6f","x72","x67"];
+document.write("<a href=\"&#x6d;&#x61;&#105;&#108;&#x74;&#x6f;&#58;");
+for (i=0; i<AkUcZUXcrbEcwQy.length; i++) document.write('&#'+AkUcZUXcrbEcwQy[i]+';');
+for (i=0; i<IgzNFFxZveFAkzb.length; i++) document.write(IgzNFFxZveFAkzb[i]);
+document.write('" >');
+for (i=0; i<VpAxDnmtwguKnou.length; i++) document.write('&#'+VpAxDnmtwguKnou[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function kHssbPiiljxQSkN()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var GXufggDmVZNSxUd=["x72","x65","103","64","100","x64","115","x6f","x63","99","x65","114","x2e","x6f","114","103"];
+var FQculJiMSumTQvY=[""];
+var ikaOeXPYpioYeqJ=["114","x65","103","x40","x64","100","115","111","x63","99","101","114","x2e","111","x72","103"];
+document.write("<a href=\"&#x6d;&#x61;&#000105;&#108;&#000116;&#x6f;&#00058;");
+for (i=0; i<GXufggDmVZNSxUd.length; i++) document.write('&#'+GXufggDmVZNSxUd[i]+';');
+for (i=0; i<FQculJiMSumTQvY.length; i++) document.write(FQculJiMSumTQvY[i]);
+document.write('" >');
+for (i=0; i<ikaOeXPYpioYeqJ.length; i++) document.write('&#'+ikaOeXPYpioYeqJ[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function PqrBLAwqXUsEcGI()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var obGjVmAzXEHcbSA=["114","x65","102","64","100","100","x73","x6f","99","x63","101","x72","46","x6f","x72","x67"];
+var QqdvSckpWSfLsSa=[""];
+var hEuZRKGxTLkjNrV=["114","101","x66","x40","100","x64","115","111","x63","99","101","114","x2e","111","x72","x67"];
+document.write("<a href=\"&#x6d;&#00097;&#000105;&#x6c;&#116;&#000111;&#58;");
+for (i=0; i<obGjVmAzXEHcbSA.length; i++) document.write('&#'+obGjVmAzXEHcbSA[i]+';');
+for (i=0; i<QqdvSckpWSfLsSa.length; i++) document.write(QqdvSckpWSfLsSa[i]);
+document.write('" >');
+for (i=0; i<hEuZRKGxTLkjNrV.length; i++) document.write('&#'+hEuZRKGxTLkjNrV[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function oApvddXtLVzKyDx()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var tPPdremuKtPQsuW=["x63","x6f","97","99","104","x65","x73","x63","x6f","x6f","x72","100","x69","110","97","x74","111","114","x40","100","100","x73","x6f","x63","x63","x65","x72","46","x6f","x72","103"];
+var lRHsOyUUvyWubao=[""];
+var DhuGLtOpVwtGeiP=["x63","x6f","97","x63","x68","101","x73","99","x6f","x6f","x72","100","105","x6e","x61","x74","111","114","x40","x64","100","x73","111","x63","99","101","114","x2e","x6f","x72","103"];
+document.write("<a href=\"&#x6d;&#x61;&#105;&#x6c;&#116;&#000111;&#x3a;");
+for (i=0; i<tPPdremuKtPQsuW.length; i++) document.write('&#'+tPPdremuKtPQsuW[i]+';');
+for (i=0; i<lRHsOyUUvyWubao.length; i++) document.write(lRHsOyUUvyWubao[i]);
+document.write('" >');
+for (i=0; i<DhuGLtOpVwtGeiP.length; i++) document.write('&#'+DhuGLtOpVwtGeiP[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function QWlNgAgBCRaHriU()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var CKrFZTeFgvaiUVf=["x67","105","114","108","115","99","x6f","x6f","114","x64","x69","110","x61","x74","x6f","x72","x40","100","100","x73","x6f","x63","x63","101","x72","46","111","114","x67"];
+var iNuLmVxLrVCNoSc=[""];
+var pOcjrBTFAgNlAWW=["103","x69","114","x6c","115","x63","x6f","x6f","x72","100","105","110","97","x74","x6f","x72","x40","100","100","115","x6f","x63","x63","x65","114","46","x6f","x72","x67"];
+document.write("<a href=\"&#x6d;&#x61;&#000105;&#x6c;&#x74;&#111;&#x3a;");
+for (i=0; i<CKrFZTeFgvaiUVf.length; i++) document.write('&#'+CKrFZTeFgvaiUVf[i]+';');
+for (i=0; i<iNuLmVxLrVCNoSc.length; i++) document.write(iNuLmVxLrVCNoSc[i]);
+document.write('" >');
+for (i=0; i<pOcjrBTFAgNlAWW.length; i++) document.write('&#'+pOcjrBTFAgNlAWW[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function hLkVFWFIoZDqRUs()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var VsikKuMFZkwnBLx=["98","111","x79","115","x63","x6f","x6f","114","100","x69","x6e","97","x74","x6f","x72","x40","100","100","115","x6f","x63","x63","x65","x72","46","x6f","x72","103"];
+var yxlVucACNGmAZrH=[""];
+var zzeFGVxYGXqVzQC=["x62","x6f","x79","x73","99","x6f","x6f","114","x64","105","x6e","97","x74","111","114","x40","x64","100","115","111","x63","x63","101","x72","46","111","114","x67"];
+document.write("<a href=\"&#x6d;&#x61;&#000105;&#108;&#116;&#x6f;&#x3a;");
+for (i=0; i<VsikKuMFZkwnBLx.length; i++) document.write('&#'+VsikKuMFZkwnBLx[i]+';');
+for (i=0; i<yxlVucACNGmAZrH.length; i++) document.write(yxlVucACNGmAZrH[i]);
+document.write('" >');
+for (i=0; i<zzeFGVxYGXqVzQC.length; i++) document.write('&#'+zzeFGVxYGXqVzQC[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
+
+function SazFVMvjigoivix()
+{
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */ 
+var DZOjoXKsuvAhIHR=["x77","x65","x62","x6d","97","115","116","101","x72","x40","x64","x64","115","x6f","99","x63","101","x72","x2e","x6f","114","103"];
+var CDfesVvIhFGbQrB=[""];
+var pKAkumGwTBRaPwW=["119","x65","98","109","x61","115","x74","x65","x72","64","x64","x64","115","x6f","99","99","101","x72","46","111","x72","103"];
+document.write("<a href=\"&#x6d;&#x61;&#x69;&#108;&#x74;&#111;&#58;");
+for (i=0; i<DZOjoXKsuvAhIHR.length; i++) document.write('&#'+DZOjoXKsuvAhIHR[i]+';');
+for (i=0; i<CDfesVvIhFGbQrB.length; i++) document.write(CDfesVvIhFGbQrB[i]);
+document.write('" >');
+for (i=0; i<pKAkumGwTBRaPwW.length; i++) document.write('&#'+pKAkumGwTBRaPwW[i]+';');
+document.write('</a>');
+/* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
